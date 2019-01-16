f598d9e690c3d144a51faf084201b153c6316340
Jon Huynh <jhsoccer123@gmail.com>
2019-01-15 23:41:46 -0500

    Added LoginVC. End of lesson 4.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Jon Huynh on 1/15/19.
+//  Copyright © 2019 Jon Huynh. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Jon Huynh on 1/15/19.
+//  Copyright © 2019 Jon Huynh. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
