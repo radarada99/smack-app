6d51ecf0458bc5bb65bef9975c645ad7bcca8322
Jon Huynh <jhsoccer123@gmail.com>
2019-01-16 23:36:44 -0500

    Created our three authservice constants and created register user function. End of Lesson 6.

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 
-// Segues
+typealias CompletionHandler = (_ Success: Bool) -> () // Created a simple custom closure
 
+// URL Constants
+let BASE_URL = "https://chattychatjh.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
