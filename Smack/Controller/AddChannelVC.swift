34a86fc903b0b2e4f6b07ecd5d12e888c1830f8b
Jon Huynh <jhsoccer123@gmail.com>
2019-02-05 09:47:38 -0500

    Added modal ChannelVC and XIB file to create the popup for adding a new channel. End of lesson 18.

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,43 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Jon Huynh on 2/5/19.
+//  Copyright © 2019 Jon Huynh. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceholder])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceholder])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -40,6 +40,13 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         setupUserInfo()
     }
     
+    @IBAction func addChannelPressed(_ sender: Any) {
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+    }
+    
+    
     @objc func userDataDidChange(_ notif: Notification) {
         setupUserInfo()
     }
