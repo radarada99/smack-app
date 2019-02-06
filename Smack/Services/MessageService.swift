276d5c4c3231eced3ce14990ba77480936734dac
Jon Huynh <jhsoccer123@gmail.com>
2019-02-06 16:20:48 -0500

    Created a model for the messages. Created function in MessageService to retrieve all messages for a given channel. Created more functions in ChatVC to check for channel and update view based on which channel is selected. End of lesson 21.

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -16,7 +16,7 @@ class MessageService {
     
     // Created a place to store our channels
     var channels = [Channel]()
-    
+    var messages = [Message]()
     var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
@@ -48,6 +48,39 @@ class MessageService {
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func findAllMessagesForChannel(channelId: String, completion: @escaping CompletionHandler) {
+        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                self.clearMessages()
+                guard let data = response.data else { return }
+                if let json = JSON(data: data).array {
+                    for item in json {
+                        let messageBody = item["messageBody"].stringValue
+                        let channelId = item["channelId"].stringValue
+                        let id = item["_id"].stringValue
+                        let userName = item["userName"].stringValue
+                        let userAvatar = item["userAvatar"].stringValue
+                        let userAvatarColor = item["userAvatarColor"].stringValue
+                        let timeStamp = item["timeStamp"].stringValue
+                        
+                        let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                        self.messages.append(message)
+                    }
+                    print(self.messages)
+                    completion(true)
+                }
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+    func clearMessages() {
+        messages.removeAll()
     }
     
     func clearChannels() {
